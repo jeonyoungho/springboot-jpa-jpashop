425194e18b37742b0edf53675e083e027137ec3f
전용호 <yhjun1000@gmail.com>
2021-10-29 16:41:10 +0900

    initial commit for springboot-jpa-jpashop

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+
+import com.fasterxml.jackson.databind.deser.std.StdKeyDeserializer;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class) // junit에게 spring관련된걸로 test할거야라고 알려줘야함
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional // @Transactional 어노테이션이 테스트에 있으면 테스트가 끝난 다음에 롤백해버리기에 실제 DB에는 데이터가 없다.
+    @Rollback(false) // 테스트가 끝나도 롤백하지 않도록 지정
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member); // 영속성 컨텍스트에 있는 member를 가져오기이 같은 객체를 가져오게 된다.
+        System.out.println("findMember == member: " + (findMember == member));
+    }
+
+}
